540ead1e4d981675acbc5311a77c42dd02113de9
Sukumar Vaddi <svaddi@findhelp.com>
2024-08-01 14:54:40 -0500

    tsconfig options

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), dts({ rollupTypes: true, outDir: "dist/types" })],
   build: {
     copyPublicDir: false,
     cssCodeSplit: true,
