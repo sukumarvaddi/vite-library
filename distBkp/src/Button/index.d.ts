4a7145201524df72b33fb443e8f0467b866a280f
Sukumar Vaddi <svaddi@findhelp.com>
2024-08-01 14:26:33 -0500

    refactoring for declarations

--- distBkp/src/Button/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from 'react';
-
-type ButtonProps = ComponentProps<"button"> & {
-    dangerous?: boolean;
-};
-export declare const Button: ({ className, dangerous, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
-export default Button;
--- distBkp/src/Input/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ComponentProps } from 'react';
-
-export declare const Input: ({ className, ...props }: ComponentProps<"input">) => import("react/jsx-runtime").JSX.Element;
-export default Input;
